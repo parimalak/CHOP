4915ae05e99697c7c5bb397404d5f7f57f241220
Parimala Killada <parimala.killada@rockets.utoledo.edu>
2017-08-25 12:27:38 -0400

    Update test.py

--- src/Test/test.py
+++ src/Test/test.py
@@ -6,14 +6,19 @@ Created on Fri Aug 25 11:27:47 2017
 """
 
 import unittest
-from coding_Exercise import flatten_list
+from coding_Exercise import flatten_list,serialize_tree,deserialize_tree
 
 class flatten_list_TestCase(unittest.TestCase):
     """Tests for `flatten_list`."""
 
     def test_is_flatten_list1(self):
         """"""
-        self.assertTrue(flatten_list([2,[[3,[4]], 5]]))
-
+        self.assertEqual(flatten_list([2,[[3,[4,6]], 5]]),[2,3,4,6,5])
+        self.assertEqual(flatten_list([]),[])
+    
+    def test_is_serialize_tree(self):
+        self.assertEqual(serialize_tree(deserialize_tree([1,2,3,None,4,2,None])),[1,2,3,None,4,2,None])
+        self.assertEqual(serialize_tree(deserialize_tree(([]),[])))
+ 
 if __name__ == '__main__':
     unittest.main()
